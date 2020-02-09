a84fdf33ed2bef9f7e3a31c09a7f77ede62ee215
Funya1337 <funya7654@gmail.com>
2020-02-10 00:24:23 +0300

    fix(container): change cyrillic to latin character

--- src/components/container.js
+++ src/components/container.js
@@ -5,7 +5,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import { blue } from "@material-ui/core/colors";
 
 const useStyles = makeStyles({
-  сontainer: {
+  container: {
     // backgroundImage: `url(${Image})`
     backgroundColor: "red",
     color: "green"
